886897705ba1354e3e6268346fc177b2abf18b6d
Philipp Serr <episource@gmx.de>
2016-12-15 19:09:17 +0100

    Write the test output to an optional log file

--- test/_utils.sh
+++ test/_utils.sh
@@ -22,12 +22,18 @@ function invoke-all() {
     local TESTDIR="$1"
     local PASSED_COUNT=0
     local FAILED_COUNT=0
+    
+    if [[ -z "$LOGFILE" ]]; then
+        local LOGFILE="/dev/null"
+    fi
 
     for test in $(ls $TESTDIR); do
         [[ "$(basename $test)" == "all.sh" ]] && continue
         
+        [[ -s "$LOGFILE" ]] && echo >>"$LOGFILE"
+        echo "=== TEST: $(basename $TESTDIR)/$test ===" >>"$LOGFILE"
         >&2 echo -n "TEST: $test... "
-        "$SCRIPT_DIR/$test" 2>/dev/null >/dev/null
+        "$SCRIPT_DIR/$test" &>>"$LOGFILE"
         
         if [[ $? -eq 0 ]]; then
             echo "PASSED"
@@ -36,6 +42,8 @@ function invoke-all() {
             echo "FAILED"
             (( FAILED_COUNT++ ))
         fi
+        
+        IS_FIRST=
     done
 
     echo "PASSED: $PASSED_COUNT FAILED: $FAILED_COUNT"
